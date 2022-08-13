51a9de607f41b1e3336441099e637c7db29d3a29
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-08-13 17:42:11 +0700

    Install react-icons and import fonts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1 className="text-4xl font-bold">Hello!</h1>
+      <h2 className="text-3xl">
+        I'm <span className="font-signature">Phie</span>
+      </h2>
     </div>
   );
 }
