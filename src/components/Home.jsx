ed62f42801f42dc5a26f269e21225481294aa2e6
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-09-07 13:55:20 +0700

    Implement react smooth scrolling from Home to Portfolio

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiArrowRight } from 'react-icons/fi';
+import { Link } from 'react-scroll';
 
 const Home = () => {
   return (
@@ -19,12 +20,17 @@ const Home = () => {
           </p>
 
           <div>
-            <button className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer">
+            <Link
+              to="portfolio"
+              smooth
+              duration={500}
+              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+            >
               Portfolio
               <span className="group-hover:rotate-90 duration-300 ml-1">
                 <FiArrowRight size={20} />
               </span>
-            </button>
+            </Link>
           </div>
         </div>
       </div>
