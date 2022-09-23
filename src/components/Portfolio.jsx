0ee552ac45159dc3c13ea3f87e5437205be20db0
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-09-23 17:27:06 +0700

    Update portfolio component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+import { TbWorld } from 'react-icons/tb';
 import pf1 from '../assets/portfolio/portfolio-1.png';
 import pf2 from '../assets/portfolio/portfolio-2.png';
 import pf3 from '../assets/portfolio/portfolio-3.png';
@@ -8,12 +10,42 @@ import pf6 from '../assets/portfolio/portfolio-6.png';
 
 const Portfolio = () => {
   const portfolios = [
-    { id: 1, src: pf1 },
-    { id: 2, src: pf2 },
-    { id: 3, src: pf3 },
-    { id: 4, src: pf4 },
-    { id: 5, src: pf5 },
-    { id: 6, src: pf6 },
+    {
+      id: 1,
+      src: pf1,
+      demo: 'https://phieraditya.github.io/react-dog-adoption-website/',
+      code: 'https://github.com/phieraditya/react-dog-adoption-website',
+    },
+    {
+      id: 2,
+      src: pf2,
+      demo: 'https://phieraditya.github.io/react-todo-list-app/',
+      code: 'https://github.com/phieraditya/react-todo-list-app',
+    },
+    {
+      id: 3,
+      src: pf3,
+      demo: 'https://phieraditya.github.io/react-yahtzee/',
+      code: 'https://github.com/phieraditya/react-yahtzee',
+    },
+    {
+      id: 4,
+      src: pf4,
+      demo: 'https://phieraditya.github.io/map-workouts-log/',
+      code: 'https://github.com/phieraditya/map-workouts-log',
+    },
+    {
+      id: 5,
+      src: pf5,
+      demo: 'https://phieraditya.github.io/marketing-page/',
+      code: 'https://github.com/phieraditya/marketing-page',
+    },
+    {
+      id: 6,
+      src: pf6,
+      demo: 'https://phieraditya.github.io/online-banking/',
+      code: 'https://github.com/phieraditya/online-banking',
+    },
   ];
 
   return (
@@ -28,20 +60,26 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
-            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
-              <img
-                src={src}
-                alt="Webpage"
-                className="rounded-md duration-200 hover:scale-105"
-              />
-              <div className="max-w-screen-lg mx-auto p-4 flex items-center justify-center">
-                <button className="w-1/2 px-6 py-3 m-4 duration-200">
-                  Demo
-                </button>
-                <button className="w-1/2 px-6 py-3 m-4 duration-200">
-                  Code
-                </button>
+          {portfolios.map(({ id, src, demo, code }) => (
+            <div key={id} className="duration-300 hover:scale-105">
+              <img src={src} alt="Webpage" className="rounded-t-md" />
+              <div className="max-w-screen-lg p-4 flex">
+                <a
+                  href={demo}
+                  className="w-1/2 py-2 flex justify-center duration-300  hover:text-blue-600"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <TbWorld size={25} />
+                </a>
+                <a
+                  href={code}
+                  className="w-1/2 py-2 flex justify-center duration-200  hover:text-blue-600"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <FaGithub size={25} />
+                </a>
               </div>
             </div>
           ))}
